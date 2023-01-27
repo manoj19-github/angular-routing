eb0a19b3aee2166892985b0bbf5e6147bd6c228e
Manoj Santra <santramanoj1997@gmail.com>
2023-01-27 12:37:29 +0530

    navigate programmitally

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.css']
+  styleUrls: ['./help.component.css'],
 })
 export class HelpComponent {
+  constructor(private route: Router, private activatedRoute: ActivatedRoute) {}
 
+  navigateToHome() {
+    this.route.navigate(['contact'], { relativeTo: this.activatedRoute });
+  }
 }
